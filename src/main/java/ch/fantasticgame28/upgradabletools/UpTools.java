1c7fee8b45cf8411ac8db01c3a72832666aa63ed
robin <rtmbaertschi007@gmail.com>
2022-04-24 19:13:57 +0200

    Hi

---
+++ src/main/java/ch/fantasticgame28/upgradabletools/UpTools.java
@@ -0,0 +1,10 @@
+package ch.fantasticgame28.upgradabletools;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UpTools implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
